e420d808e993994b77d45397a284133d0def2b09
2395171302@qq.com <2395171302@qq.com>
2018-11-19 14:11:40 +0800

    当前分支是dev,这是第五次提交！

--- ssh01/src/main/java/com/Test.java
+++ ssh01/src/main/java/com/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		System.out.println("当前分支是eclipse创建分支dev！，这是我第四次提交");
 		System.out.println("当前分支是eclise创建的分支！没有提交到本地仓库");
 		System.out.println("当前分支是master分支没有提交到本地仓库");
+		System.out.println("当前分支是dev,这是第五次提交！");
 	}
 
 }
