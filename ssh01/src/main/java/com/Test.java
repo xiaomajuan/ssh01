032e8e979960abeffa74333fa950f09ba2c8dbd6
2395171302@qq.com <2395171302@qq.com>
2018-11-19 12:44:38 +0800

    当前分支是eclise创建的分支！没有提交到本地仓库

--- ssh01/src/main/java/com/Test.java
+++ ssh01/src/main/java/com/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		System.out.println("这是第二次提交！");
 		System.out.println("当前分支是dev分支，这是我第三次提交！");
 		System.out.println("当前分支是eclipse创建分支dev！，这是我第四次提交");
+		System.out.println("当前分支是eclise创建的分支！没有提交到本地仓库");
+		System.out.println("当前分支是master分支没有提交到本地仓库");
 	}
 
 }
